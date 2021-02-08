d69f67e479fcea564f2db1d98279c1b730b938f8
sayeed89 <37158975+sayeed89@users.noreply.github.com>
2021-02-08 17:00:32 +0530

    Update app-id-sauce.sh

--- app-id-sauce.sh
+++ app-id-sauce.sh
@@ -4,3 +4,6 @@ SAUCE_APP_API_KEY='504ADDD26B41488B8BEBD3F839A086A6'
 TEST_APP_ID=$(curl -u "$SAUCE_USERNAME:$SAUCE_APP_API_KEY" -X POST https://app.testobject.com:443/api/storage/upload -H "Content-Type: application/octet-stream" --data-binary @./calcy.ipa 2>/dev/null)
 
 echo "$TEST_APP_ID"
+
+echo 'export TEST_APP_ID=$(git log -1 --pretty=format:"%s")' >> $BASH_ENV
+source $BASH_ENV
